65ff496fd1c0101d80d917e6bb6a25219fc2b651
Jason Madden <jamadden@gmail.com>
2017-07-31 10:08:38 -0500

    Preparing release 4.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.copy',
-      version='4.1.0.dev0',
+      version='4.1.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Pluggable object copying mechanism',
