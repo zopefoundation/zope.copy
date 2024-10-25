c3463e3faaf364dbd98a80332e9025cf3749c2cf
Michael Howitz <icemac@gmx.net>
2024-10-25 08:20:47 +0200

    Preparing release 5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-5.0 (unreleased)
+5.0 (2024-10-25)
 ================
 
 - Add support for Python 3.12, 3.13.
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.copy',
-      version='5.0.dev0',
+      version='5.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Pluggable object copying mechanism',
