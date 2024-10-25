f56b2f4724409bc2449a107562573b792f3a8904
Michael Howitz <icemac@gmx.net>
2024-10-25 08:21:28 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.copy',
-      version='5.0',
+      version='5.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Pluggable object copying mechanism',
