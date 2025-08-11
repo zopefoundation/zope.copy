18061fabff1fd96c7cddd2eabdf8acff060222fd
Michael Howitz <icemac@gmx.net>
2025-08-11 10:38:28 +0200

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.copy',
-      version='5.1.dev0',
+      version='6.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Pluggable object copying mechanism',
