3a35307eeea0ff84d51b9edf5a0d420c8e544cb9
Michael Howitz <icemac@gmx.net>
2025-09-12 09:03:02 +0200

    Preparing release 6.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.copy',
-      version='6.0.dev0',
+      version='6.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Pluggable object copying mechanism',
